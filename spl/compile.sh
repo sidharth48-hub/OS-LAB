273a3d1e9a133f1d3bc2f2dce4766dcbc7af2b61
sidharth48-hub <sidharth.cp48@gmail.com>
2022-10-30 19:48:21 +0530

    first commit

---
+++ spl/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+./spl ./spl_progs/joel/console.spl
+./spl ./spl_progs/joel/exhandler.spl
+./spl ./spl_progs/joel/int11.spl
+./spl ./spl_progs/joel/int7.spl
+./spl ./spl_progs/joel/int9.spl
+./spl ./spl_progs/joel/module_1.spl
+./spl ./spl_progs/joel/module_4.spl
+./spl ./spl_progs/joel/timer.spl
+./spl ./spl_progs/joel/disk.spl
+./spl ./spl_progs/joel/int10.spl
+./spl ./spl_progs/joel/int6.spl
+./spl ./spl_progs/joel/int8.spl
+./spl ./spl_progs/joel/module_0.spl
+./spl ./spl_progs/joel/module_2.spl
+./spl ./spl_progs/joel/module_5.spl
---
+++ xsm/run_stage_24.sh
@@ -0,0 +1,2 @@
+cd ../xfs-interface
+./xfs-interface run ../spl/spl_progs/stage25/all.batch
