38df1d8de6e277c9f80c21731a2754fba755d723
sidharth48-hub <sidharth.cp48@gmail.com>
2022-11-01 15:33:08 +0530

    stage 26 completed

---
+++ xsm/run_stage.sh
@@ -0,0 +1,3 @@
+cd ../xfs-interface
+./xfs-interface run ../spl/spl_progs/stage26/all.batch
+echo "Loaded"
--- xsm/run_stage_24.sh
+++
@@ -1,2 +0,0 @@
-cd ../xfs-interface
-./xfs-interface run ../spl/spl_progs/stage25/all.batch
